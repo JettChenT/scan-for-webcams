75504c4625f9efded4956c4179edc36a13e67622
JettChenT <jettchen12345@gmail.com>
2020-07-27 08:02:46 +0800

    Update MJPG.py and webcamXP.py: better search queries and format code using black

--- MJPG.py
+++ MJPG.py
@@ -3,19 +3,19 @@ import shodan
 import requests
 from rich import print
 
-SHODAN_API_KEY = os.getenv("SHODAN_API_KEY") 
+SHODAN_API_KEY = os.environ.get("SHODAN_API_KEY")
+print(f"[blue]API key:[/blue][i]{SHODAN_API_KEY}[/i]")
 api = shodan.Shodan(SHODAN_API_KEY)
-try:
-    results = api.search('webcams')
-except:
-    print("error!")
+results = api.search("webcams")
 
-for result in results['matches']:
-    if "MJPG-streamer" in result['data']:
+for result in results["matches"]:
+    if "MJPG-streamer" in result["data"]:
         url = f"http://{result['ip_str']}:{result['port']}"
         try:
-            r = requests.get(url,timeout=5)
+            r = requests.get(url, timeout=5)
             if r.status_code == 200:
-                print(f"[link={url}][i][green]{result['ip_str']}[/green]:[red]{result['port']}[/red][/link]")
+                print(
+                    f"[link={url}][i][green]{result['ip_str']}[/green]:[red]{result['port']}[/red][/link]"
+                )
         except:
             continue
--- webcamXP.py
+++ webcamXP.py
@@ -1,21 +1,22 @@
-import shodan
 import os
+import shodan
 import requests
 from rich import print
 
-SHODAN_API_KEY = os.getenv("SHODAN_API_KEY") 
-api = shodan.Shodan(SHODAN_API_KEY)
-try:
-    results = api.search('webcam')
-except:
-    print("error!")
 
-for result in results['matches']:
-    if "webcamXP" in result['data']:
+SHODAN_API_KEY = os.environ.get("SHODAN_API_KEY")
+print(f"[blue]API key:[/blue][i]{SHODAN_API_KEY}[/i]")
+api = shodan.Shodan(SHODAN_API_KEY)
+results = api.search("webcams")
+
+for result in results["matches"]:
+    if "webcamXP" in result["data"]:
         url = f"http://{result['ip_str']}:{result['port']}"
         try:
-            r = requests.get(url,timeout=5)
+            r = requests.get(url, timeout=5)
             if r.status_code == 200:
-                print(f"[link={url}][i][green]{result['ip_str']}[/green]:[red]{result['port']}[/red][/link]")
+                print(
+                    f"[link={url}][i][green]{result['ip_str']}[/green]:[red]{result['port']}[/red][/link]"
+                )
         except:
             continue
