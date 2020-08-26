e8ca953fadbf3f181b41eeb0305c4f6ad2402e85
JettChenT <jettchen12345@gmail.com>
2020-08-26 09:40:59 +0800

    fix bug

--- search.py
+++ search.py
@@ -22,7 +22,7 @@ class Scanner(object):
     def init_clarifai(self):
         self.CLARIFAI_API_KEY = os.environ.get("CLARIFAI_API_KEY")
         assert self.CLARIFAI_API_KEY != ""
-        self.clarifai_app = ClarifaiApp(self.CLARIFAI_API_KEY)
+        self.clarifai_app = ClarifaiApp(api_key=self.CLARIFAI_API_KEY)
         self.clarifai_model = self.clarifai_app.public_models.general_model
         self.clarifai_initialized = True
 
